39c504ab5e359e0ef3fc1ced48a03d0ec73a3623
Tsekhmister Alexander <alant2212@gmail.com>
2021-12-21 14:58:01 +0300

    Suffering around ICMP

--- kern/icmp.c
+++ kern/icmp.c
@@ -6,6 +6,31 @@
 #include <kern/icmp.h>
 #include <inc/stdio.h>
 
+int16_t get_checksum(int8_t data[], int32_t length)
+{
+  int i = 0;
+  int32_t temp = 0;
+  int16_t checksum = 0;
+
+  while (i < length)
+  {
+    if ((i+1) < length)
+    {
+      temp = (int32_t)checksum + ((((int32_t)data[i])<<8) + data[i+1]);
+      checksum = (temp & 0xffff) + (temp >> 16);
+      i = i + 2;
+    }
+    else
+    {
+      temp = checksum  + (data[i] << 8);
+      checksum = (temp & 0xffff) + (temp >> 16);
+      i = i + 1;
+    }
+  }
+  checksum = ~checksum;
+  return checksum;
+}
+
 int
 icmp_echo_reply(struct ip_pkt* pkt) {
     struct icmp_pkt icmp_packet;
@@ -24,7 +49,7 @@ icmp_echo_reply(struct ip_pkt* pkt) {
     hdr->sequence_number = JHTONS(hdr->sequence_number);
 
     hdr->checksum = 0;
-    hdr->checksum = ip_checksum(&icmp_packet, sizeof(icmp_packet));
+    hdr->checksum = get_checksum((void*)&icmp_packet, sizeof(icmp_packet));
 
     struct ip_pkt result;
     result.hdr.ip_protocol = IP_PROTO_ICMP;
