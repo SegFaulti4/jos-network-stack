330dfcbb6c7468449789a493c884718693f83b9d
Andrey Ezhov <ezhovandr@gmail.com>
2021-12-24 22:12:12 +0400

    udp tests

--- itask_utility/udp_send.c
+++ itask_utility/udp_send.c
@@ -6,21 +6,29 @@
 #include <stdio.h>
 
 int main(void) {
-	int sockfd = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
-	struct hostent *me = gethostbyname("192.168.123.2");//gethostbyname("192.168.123.2");
-	struct sockaddr_in addr = {
-		.sin_family = AF_INET,
-		.sin_addr = *(struct in_addr *)me->h_addr_list[0],
-		.sin_port = htons(8888)
-	};
+    int sockfd = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    struct hostent *me = gethostbyname("192.168.123.1");
+    struct sockaddr_in my_addr = {
+        .sin_family = AF_INET,
+        .sin_addr = *(struct in_addr *)me->h_addr_list[0],
+        .sin_port = htons(1234)
+    };
+    bind(sockfd, (struct sockaddr *)&my_addr, sizeof(my_addr));
 
-	getchar();
-	while (1) {
-	//for (int i = 0; i < 70; i++) {
-		char buf[] = "Hello\nI am glad to send a very long UDP package\n";
-		sendto(sockfd, buf, sizeof(buf), 0, (struct sockaddr *)&addr, sizeof(addr));
-		printf("send\n");
-		getchar();
-	}
-	return 0;
-}
+    struct hostent *jos = gethostbyname("192.168.123.2");
+    struct sockaddr_in jos_addr = {
+        .sin_family = AF_INET,
+        .sin_addr = *(struct in_addr *)jos->h_addr_list[0],
+        .sin_port = htons(8888)
+    };
+
+    char buf[] = "HELLO";
+    sendto(sockfd, buf, sizeof(buf), 0, (struct sockaddr *)&jos_addr, sizeof(jos_addr));
+
+    char reply[1024] = {};
+    recvfrom(sockfd, reply, sizeof(reply), 0, NULL, NULL);
+    printf("%s", reply);
+
+    close(sockfd);
+    return 0;
+} 
