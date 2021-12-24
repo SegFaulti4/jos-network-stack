ecf7ba4f37afde1d92a1f28816179143b2d108e0
Tsekhmister Alexander <alant2212@gmail.com>
2021-12-24 22:44:38 +0300

    JOS output check + JOS running

--- itask_utility/test.sh
+++ itask_utility/test.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-make udp_send_recv
+jobs=()
+trap 'kill $jobs' EXIT HUP TERM INT
+make -s udp_send_recv
+NETWORK_ENABLE=1 make -C .. qemu 2>/dev/null 1>out.txt & jobs+=($!)
+sleep 20
 python3 tests.py
+if cat out.txt | grep -q "HELLO"; then echo "JOS UDP output - OK"; else echo "JOS UDP output - FAIL"; fi
+rm out.txt
